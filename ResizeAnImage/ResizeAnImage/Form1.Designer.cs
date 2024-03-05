3c00004ce26700dd77a1397a3077c3a9caccfe50
HellMonsteRBG <georgikolev377@gmail.com>
2024-03-05 15:01:19 +0200

    Upload all files

---
+++ ResizeAnImage/ResizeAnImage/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace ResizeAnImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonImageSelect = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(150, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(500, 27);
+            this.textBox1.TabIndex = 0;
+            // 
+            // buttonImageSelect
+            // 
+            this.buttonImageSelect.Location = new System.Drawing.Point(335, 114);
+            this.buttonImageSelect.Name = "buttonImageSelect";
+            this.buttonImageSelect.Size = new System.Drawing.Size(130, 29);
+            this.buttonImageSelect.TabIndex = 1;
+            this.buttonImageSelect.Text = "Select Image";
+            this.buttonImageSelect.UseVisualStyleBackColor = true;
+            this.buttonImageSelect.Click += new System.EventHandler(this.buttonImageSelect_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(200, 160);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Location = new System.Drawing.Point(405, 380);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(100, 29);
+            this.buttonGenerate.TabIndex = 3;
+            this.buttonGenerate.Text = "Generate";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(295, 380);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 27);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Image path:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(174, 383);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Minify image %:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(614, 373);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(125, 27);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(614, 414);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(125, 27);
+            this.textBox4.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 453);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.buttonGenerate);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonImageSelect);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Image Resizer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button buttonImageSelect;
+        private PictureBox pictureBox1;
+        private Button buttonGenerate;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+    }
+}
---
+++ ResizeAnImage/ResizeAnImage/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace ResizeAnImage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string pathOfImage = "";
+
+       
+        static void getDecimalRGB(int[,] pixelsArray, Bitmap image)
+        {
+            const int PixelWidth = 3;
+            const PixelFormat PixelFormat = PixelFormat.Format24bppRgb;
+            
+            BitmapData data = image.LockBits(new Rectangle(0, 0, image.Width-1, image.Height-1), System.Drawing.Imaging.ImageLockMode.ReadOnly, PixelFormat);
+            try
+            {
+                byte[] pixelData = new Byte[data.Stride];
+                for (int scanline = 0; scanline < data.Height; scanline++)
+                {
+                    Marshal.Copy(data.Scan0 + (scanline * data.Stride), pixelData, 0, data.Stride);
+                    for (int pixeloffset = 0; pixeloffset < data.Width; pixeloffset++)
+                    {
+                        pixelsArray[scanline,pixeloffset] =
+                            ((pixelData[pixeloffset * PixelWidth + 2] << 16) + 
+                            (pixelData[pixeloffset * PixelWidth + 1] << 8) + 
+                            pixelData[pixeloffset]);
+                    }
+                }
+            }
+            finally
+            {
+                image.UnlockBits(data);
+            }
+        }
+
+        public void MinifyArray(int[,] mimimisedArray, int[,] pixelsArray, int height, int width, int pixelHeight, int pixelWidth)
+        {
+            double percentage = double.Parse(textBox2.Text) / 100;
+            double divisionCount = (double)(1 / percentage);
+            decimal avgRed = 0;
+            decimal avgGreen = 0;
+            decimal avgBlue = 0;
+            decimal avgDecColor = 0;
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    for (int k =((int)(Math.Round(i*divisionCount))); k < ((int)(Math.Round((i+1)* divisionCount))); k++)
+                    {
+                        for (int l = ((int)(Math.Round(j*divisionCount))); l < ((int)(Math.Round((j+1)* divisionCount))); l++)
+                        {
+                            avgDecColor = pixelsArray[k, l];
+                            avgBlue +=avgDecColor%256;
+                            avgDecColor /= 256;
+                            avgGreen += avgDecColor % 256;
+                            avgDecColor /= 256;
+                            avgRed += avgDecColor;
+                        
+                        }
+                    }
+                    avgBlue = (int)(avgBlue / (int)(Math.Round(divisionCount*divisionCount)));
+                    avgGreen = (int)(avgGreen / (int)(Math.Round(divisionCount * divisionCount)));
+                    avgRed = (int)(avgRed / (int)(Math.Round(divisionCount * divisionCount)));
+
+                    mimimisedArray[i,j] = (int)((avgRed*256+avgGreen)*256+avgBlue);
+                    avgBlue = 0;
+                    avgGreen = 0;
+                    avgRed = 0;
+                   // textBox3.Text = (avgColor / (minimisedHeightPixels * minimisedWidthPixels)).ToString();
+                }
+            }
+
+            //textBox3.Text = mimimisedArray[3, 3].ToString();
+        }
+
+        public void createPicture(int[,] pixelsArray, int width, int height)
+        {
+            Bitmap newImage = new Bitmap(width, height);
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    int index = y * width + x;
+
+                    int rgbValue = pixelsArray[y, x];
+
+                    int r = (rgbValue >> 16) & 0xFF; 
+                    int g = (rgbValue >> 8) & 0xFF;  
+                    int b = rgbValue & 0xFF;         
+
+                    newImage.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+            }
+            newImage.Save(@"D:\MyImage.png", ImageFormat.Png);
+        }
+
+        private void buttonImageSelect_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Title = "Select Image";
+            openFileDialog1.InitialDirectory = @"C:\";
+            openFileDialog1.Filter = "Image files (*.jpg; *.jpeg; *.png; *.bmp;) | *.jpg; *.jpeg; *.png; *.bmp";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName != "")
+            {
+                pathOfImage = openFileDialog1.FileName;
+                textBox1.Text = pathOfImage;
+                pictureBox1.Image = new Bitmap(pathOfImage);
+            }
+            else
+            {
+                textBox1.Text = "You didn't select an image!";
+            }
+        }
+
+        private void buttonGenerate_Click(object sender, EventArgs e)
+        {
+            Bitmap foo = Bitmap.FromFile(pathOfImage) as Bitmap;
+            int height = foo.Height;
+            int width = foo.Width;
+            int[,] pixelsArray = new int[height, width];
+            getDecimalRGB(pixelsArray, foo);
+            int minimisedHeight = height * int.Parse(textBox2.Text) / 100;
+            int minimisedWidth = width * int.Parse(textBox2.Text) / 100;
+            int[,] mimimisedArray = new int[minimisedHeight, minimisedWidth];
+            //createPicture(pixelsArray, foo.Width, foo.Height);
+            MinifyArray(mimimisedArray, pixelsArray, minimisedHeight, minimisedWidth,height, width);
+            createPicture(mimimisedArray, minimisedWidth, minimisedHeight);
+
+
+        }
+    }
+}
